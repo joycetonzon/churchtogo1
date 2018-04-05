21b4b3a514ae2a8abd2c6ee1a987b0b4a419b91e
joycetonzon <Najtonzon06@gmail.com>
2018-04-06 01:06:20 +0800

    .

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 require('dotenv').config();
 
+require("./app")(app)
+
 var app = express();
-require("/app")(app)
 
-
-// var func = require('/app/index.js');
+var func = require('/app/index.js');
 // func (app);
 app.listen(app.get('port'), () => {
     console.log(`My server is listening to PORT: ${app.get('port')}`);
