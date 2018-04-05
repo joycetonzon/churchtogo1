58056123c5a3212db344aad80ca4b4f672965368
joycetonzon <Najtonzon06@gmail.com>
2018-04-05 23:56:08 +0800

    .

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var express = require('express');
 
 var app = express();
 
-var func=require('./app');
-func(app);
+var func = require('./app/index.js');
+ func (app);
 app.listen(app.get('port'), () => {
     console.log(`My server is listening to PORT: ${app.get('port')}`);
 });
